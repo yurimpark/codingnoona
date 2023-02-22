28d048f7806116e81aa31e3ed41c8fdc102e5c2d
Yurim Park <99720055+yurimpark@users.noreply.github.com>
2023-02-22 14:50:50 +0900

    가위바위보 App.js

---
+++ rockpaperscissor/src/App.js
@@ -0,0 +1,48 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Box from "./component/Box";
+
+// 1. 유저는 박스 두개를 볼 수 있다.(타이틀, 사진, 결과).
+// 2. 유저는 박스 하단에 가위바위보 버튼을 볼 수 있다.
+// 3. 버튼을 클릭하면 클릭한 아이템이 유저 박스에 보인다.
+// 4. 버튼을 클릭하면 컴퓨터 아이템은 랜덤하게 선택이 된다.
+// 5. 3번 4번의 아이템을 가지고 누가 이겼는지 승패를 나눈다.
+// 6. 박스 테두리가 결과에 따라 색이 변한다. 지면 빨간색, 이기면 초록색, 비기면 검정색이 보인다.
+
+const choice = {
+  rock: {
+    name: "Rock",
+    img: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FpSJwo%2FbtqXJV1lACE%2Fnx5XrxkCLWXh9UsnoS8vbK%2Fimg.png",
+  },
+  scissors: {
+    name: "Scissors",
+    img: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FHfURw%2FbtqXKvOTNWK%2FgWTwPXEg9QzSV0ilOuwuak%2Fimg.png",
+  },
+  paper: {
+    name: "Paper",
+    img: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbmjB2s%2FbtqXHhp6kpG%2FTH14W4U612SxKo9uuR2sB0%2Fimg.png",
+  },
+};
+
+function App() {
+  const play = (userChoice) => {
+    console.log("선택됨", userChoice);
+  };
+
+  return (
+    <div>
+      <div className="main">
+        <Box title="You" />
+        <Box title="Computer" />
+      </div>
+      <div className="main">
+        {/* 함수를 콜백형태로 전달해주는 것이 중요 */}
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
