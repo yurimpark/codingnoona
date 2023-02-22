55af9718fea539fc2a883c65524e4448f0e34686
Yurim Park <99720055+yurimpark@users.noreply.github.com>
2023-02-22 14:50:11 +0900

    가위바위보 박스 컴포넌트

---
+++ rockpaperscissor/src/component/Box.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Box = (props) => {
+  return (
+    <div className="box">
+      <h1>{props.title}</h1>
+      <img
+        className="item-img"
+        src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FHfURw%2FbtqXKvOTNWK%2FgWTwPXEg9QzSV0ilOuwuak%2Fimg.png"
+      />
+      <h2>WIN</h2>
+    </div>
+  );
+};
+
+export default Box;
