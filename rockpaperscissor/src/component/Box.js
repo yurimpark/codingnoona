d96b544b1393f6a7b8960f12ce5f80692f455b7d
Yurim Park <99720055+yurimpark@users.noreply.github.com>
2023-02-22 22:41:13 +0900

    유저와 컴퓨터의 결과 반대로 보여주기.

--- rockpaperscissor/src/component/Box.js
+++ rockpaperscissor/src/component/Box.js
@@ -1,11 +1,23 @@
 import React from "react";
 
 const Box = (props) => {
-  console.log("props", props);
-  console.log("props.item", props.item);
+  let result;
+  if (
+    props.title === "Computer" &&
+    props.result !== "tie" &&
+    props.result !== ""
+  ) {
+    // 카드가 computer카드인가? && 결과가 비긴건 아닌가? && props.result에 값이 있는가?
+    result = props.result === "win" ? "lose" : "win";
+  } else {
+    // 위의 경우가 아니라면 props로 전달된 결과를 그대로 쓴다.
+    result = props.result;
+  }
+  // console.log("props", props);
+  // console.log("props.item", props.item);
   //console.log("props.item.img", props.item.img);
   return (
-    <div className="box">
+    <div className={`box ${result}`}>
       <h1>{props.title}</h1>
       {/* const [userSelect, setUserSelect] = useState(null);
         props가 null이기 때문에 item이 없음!
@@ -14,7 +26,7 @@ const Box = (props) => {
         true니까 다음 값 확인하는데, 전체가 다 true면 맨 마지막 값을 리턴하기 때문에
         props.item.img 를 찾아서 src로 넣어주는 것! */}
       <img className="item-img" src={props.item && props.item.img} />
-      <h2>WIN</h2>
+      <h2>{result}</h2>
     </div>
   );
 };
