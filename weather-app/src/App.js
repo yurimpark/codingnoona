b47bcc19b63387a2e8e9dcfcf3611cc53656d35f
Yurim Park <99720055+yurimpark@users.noreply.github.com>
2023-02-23 13:09:55 +0900

    현재 위치로 날씨 가져오기

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,7 +1,45 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import WeatherBox from "./component/WeatherBox";
+import WeatherButton from "./component/WeatherButton";
+
+// 1. 앱이 실행되자마자 현재위치 기반의 날씨가 보인다.
+// 2. 날씨정보에는 도시, 섭씨, 화씨, 날씨상태
+// 3. 5개의 버튼이 있다.(1개는 현재위치, 4개는 다른도시)
+// 4. 도시버튼을 클릭할 때마다 도시별 날씨가 나온다.
+// 5. 현재위치 버튼을 누르면 다시 현재위치 기반의 날씨가 나온다.
+// 6. 데이터를 들고오는 동안 로딩 스피너가 돈다.
 
 function App() {
-  return <div></div>;
+  const [weather, setWeather] = useState(null);
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      getWeatherByCurrentLocation(lat, lon);
+      console.log("현재위치", lat, lon);
+    });
+  };
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=8c1966f9f037a096abbd6a5c1c57d982`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setWeather(data);
+  };
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+  return (
+    <div>
+      <div className="container">
+        <WeatherBox weather={weather} />
+        <WeatherButton />
+      </div>
+    </div>
+  );
 }
 
 export default App;
