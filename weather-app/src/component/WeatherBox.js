9e2fe4a00af4bc456c2b919bf3b9e0d440cfb93d
Yurim Park <99720055+yurimpark@users.noreply.github.com>
2023-02-23 16:05:44 +0900

    화씨 소숫점 제거

--- weather-app/src/component/WeatherBox.js
+++ weather-app/src/component/WeatherBox.js
@@ -7,7 +7,8 @@ const WeatherBox = ({ weather }) => {
       {/* 옵셔널 체이닝: weather && 와 같은 역할! */}
       <div>{weather?.name}</div>
       <h2>
-        {weather?.main.temp}°C | {(weather?.main.temp + 40) * 1.8 - 40}°F
+        {weather?.main.temp}°C |{" "}
+        {Math.round((weather?.main.temp + 40) * 1.8 - 40)}°F
       </h2>
       <h3>{weather?.weather[0].description}</h3>
     </div>
