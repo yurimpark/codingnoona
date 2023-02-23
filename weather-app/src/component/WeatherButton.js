8f58d085b13644ec1d90b5112e06f769f874a993
Yurim Park <99720055+yurimpark@users.noreply.github.com>
2023-02-23 16:20:57 +0900

    도시 버튼 수정

--- weather-app/src/component/WeatherButton.js
+++ weather-app/src/component/WeatherButton.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 
-function WeatherButton({ cities }) {
+function WeatherButton({ cities, setCity }) {
   console.log(cities);
   return (
     <div>
       <Button variant="warning">Current Location</Button>{" "}
       {/* map함수를 이용하여 버튼을 여러개 만들 필요 없이 한번에 생성 */}
       {cities.map((item) => (
-        <Button variant="info">{item}</Button>
+        <Button
+          variant="info"
+          onClick={() => {
+            // 클릭한 도시로 바꿔줌
+            setCity(item);
+          }}
+        >
+          {item}
+        </Button>
       ))}
     </div>
   );
