ce148d2c13e2567e8973b310c3561de547ee6ecb
Yurim Park <99720055+yurimpark@users.noreply.github.com>
2023-02-23 21:11:00 +0900

    섭씨 화씨 가름막 이모지로 수정

--- weather-app/src/component/WeatherBox.js
+++ weather-app/src/component/WeatherBox.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const WeatherBox = ({ weather }) => {
-  console.log(weather);
+  console.log("weather", weather);
   return (
     <div className="weather-box">
       {/* 옵셔널 체이닝: weather && 와 같은 역할! */}
       <div>{weather?.name}</div>
       <h2>
-        {weather?.main.temp}°C |{" "}
+        {weather?.main.temp}°C 🌡{" "}
         {Math.round((weather?.main.temp + 40) * 1.8 - 40)}°F
       </h2>
       <h3>{weather?.weather[0].description}</h3>
