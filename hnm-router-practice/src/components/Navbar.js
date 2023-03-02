a8521f5b3e22714d37b67b6f0a03fdb186ee6cf9
yurimpark <yuriming112@gmail.com>
2023-03-02 17:54:23 +0900

    [생성] Navbar 컴포넌트

---
+++ hnm-router-practice/src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+  const menuList = [
+    "여성",
+    "Divided",
+    "남성",
+    "신생아/유아",
+    "아동",
+    "H&M Home",
+    "Sale",
+    "지속가능성",
+  ];
+
+  return (
+    <div>
+      <div>
+        <div className="login-button">
+          <FontAwesomeIcon icon={faUser} />
+          <div>로그인</div>
+        </div>
+      </div>
+      <div className="nav-section">
+        <img
+          width={100}
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/2560px-H%26M-Logo.svg.png"
+          alt=""
+        />
+      </div>
+      <div className="menu-area">
+        <ul className="menu-list">
+          {menuList.map(menu => (
+            <li>{menu}</li>
+          ))}
+        </ul>
+        <div className="search-area">
+          <FontAwesomeIcon icon={faSearch} />
+          <input type="text" name="" id="" placeholder="제품검색" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
