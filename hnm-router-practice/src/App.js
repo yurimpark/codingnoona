c62f40ea770623eacd9b08b06c27c32b5f3692c4
yurimpark <yuriming112@gmail.com>
2023-03-03 15:17:48 +0900

    [추가] 로그인 확인 기능

--- hnm-router-practice/src/App.js
+++ hnm-router-practice/src/App.js
@@ -4,11 +4,13 @@ import ProductAll from "./pages/ProductAll";
 import Login from "./pages/Login";
 import ProductDetail from "./pages/ProductDetail";
 import Navbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState, useEffect } from "react";
 
 // 1. 전체 상품 페이지, 로그인, 상품 상세페이지
 // 1-1. 네비게이션바
-// todo 2. 전체 상품페이지에서는 전체 상품을 볼 수 있다.
-// todo 3. 로그인 버튼을 누르면 로그인 페이지가 나온다.
+// 2. 전체 상품페이지에서는 전체 상품을 볼 수 있다.
+// 3. 로그인 버튼을 누르면 로그인 페이지가 나온다.
 // todo 4. 상품 디테일을 눌렀으나, 로그인이 되지 않은 경우에는 로그인페이지가 먼저 나온다.
 // todo 5. 로그인이 되어있을 경우에는 상품디테일 페이지를 볼 수 있다.
 // todo 6. 로그아웃 버튼을 클릭하면 로그아웃이 된다.
@@ -17,12 +19,19 @@ import Navbar from "./components/Navbar";
 // todo 9. 상품을 검색할 수 있다.
 
 function App() {
+  const [authenticate, setAuthenticate] = useState(false);
+  useEffect(() => {
+    console.log("aaa", authenticate);
+  }, [authenticate]);
   return (
     <div>
       <Navbar />
       <Routes>
         <Route path="/" element={<ProductAll />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/login"
+          element={<Login setAuthenticate={setAuthenticate} />}
+        />
         <Route path="/product/:id" element={<ProductDetail />} />
       </Routes>
     </div>
