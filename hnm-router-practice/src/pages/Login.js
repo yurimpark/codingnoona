ac945773a73528ff1c04bb20b0170b297547b846
yurimpark <yuriming112@gmail.com>
2023-03-03 15:14:47 +0900

    [추가] 로그인 화면 구현

--- hnm-router-practice/src/pages/Login.js
+++ hnm-router-practice/src/pages/Login.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  return <div>Login</div>;
+const Login = ({ setAuthenticate }) => {
+  const navigate = useNavigate();
+  const loginUser = e => {
+    console.log("dkdkdkdk");
+    e.preventDefault();
+    setAuthenticate(true);
+    navigate("/");
+  };
+  return (
+    <Container>
+      <Form onSubmit={e => loginUser(e)}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="danger" type="submit">
+          로그인
+        </Button>
+      </Form>
+    </Container>
+  );
 };
 
 export default Login;
