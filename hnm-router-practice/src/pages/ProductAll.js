3db205a09d59be9bcbd2c1180a94bb3bf8f9017a
yurimpark <yuriming112@gmail.com>
2023-03-03 15:11:52 +0900

    [추가] 상품 전체 페이지

--- hnm-router-practice/src/pages/ProductAll.js
+++ hnm-router-practice/src/pages/ProductAll.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ProductCard from "./../components/ProductCard";
+import { Container, Row, Col } from "react-bootstrap";
 
 const ProductAll = () => {
-  return <div>전체 상품 페이지</div>;
+  const [productList, setProductList] = useState([]);
+  const getProducts = async () => {
+    let url = `http://localhost:5000/products`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setProductList(data);
+  };
+  useEffect(() => {
+    getProducts();
+  });
+  return (
+    <div>
+      <Container>
+        <Row>
+          {productList.map(menu => (
+            <Col lg={3}>
+              <ProductCard item={menu} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default ProductAll;
