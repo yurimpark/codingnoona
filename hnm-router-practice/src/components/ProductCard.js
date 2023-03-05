32bc4121cf69ca53d8747e26d55a97a618bbb678
yurimpark <yuriming112@gmail.com>
2023-03-05 17:54:50 +0900

    [추가] ProductCard 클릭 시 각 상품 id로 연결

--- hnm-router-practice/src/components/ProductCard.js
+++ hnm-router-practice/src/components/ProductCard.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const ProductCard = ({ item }) => {
+  const navigate = useNavigate();
+  const showDetail = () => {
+    navigate(`/product/${item.id}`);
+  };
   return (
-    <div>
+    <div className="card" onClick={showDetail}>
       <img src={item?.img} alt="" />
       <div>{item?.choice == true ? "Conscious choice" : ""}</div>
       <div>{item?.title}</div>
