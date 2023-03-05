5824f15b33862602aa10d6a12252ac4cb68c4e92
yurimpark <yuriming112@gmail.com>
2023-03-06 01:07:24 +0900

    [추가] 상품 검색 기능

--- hnm-router-practice/src/pages/ProductAll.js
+++ hnm-router-practice/src/pages/ProductAll.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "./../components/ProductCard";
 import { Container, Row, Col } from "react-bootstrap";
+import { useSearchParams } from "react-router-dom";
 
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
+  const [query, setQuery] = useSearchParams();
+
   const getProducts = async () => {
-    let url = `http://localhost:5000/products`;
+    let searchQuery = query.get("q") || "";
+    console.log("쿼리값은", searchQuery);
+    let url = `http://localhost:5000/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
     setProductList(data);
   };
   useEffect(() => {
     getProducts();
-  });
+  }, [query]);
   return (
     <div>
       <Container>
