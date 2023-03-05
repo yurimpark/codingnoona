dabbe3b7c16388a71f54c09f9c321c5212d0e666
yurimpark <yuriming112@gmail.com>
2023-03-05 17:56:30 +0900

    [수정] 상품상세페이지 PrivateRoute로 변경

--- hnm-router-practice/src/App.js
+++ hnm-router-practice/src/App.js
@@ -6,12 +6,13 @@ import ProductDetail from "./pages/ProductDetail";
 import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState, useEffect } from "react";
+import PrivateRoute from "./route/PrivateRoute";
 
 // 1. 전체 상품 페이지, 로그인, 상품 상세페이지
 // 1-1. 네비게이션바
 // 2. 전체 상품페이지에서는 전체 상품을 볼 수 있다.
 // 3. 로그인 버튼을 누르면 로그인 페이지가 나온다.
-// todo 4. 상품 디테일을 눌렀으나, 로그인이 되지 않은 경우에는 로그인페이지가 먼저 나온다.
+// 4. 상품 디테일을 눌렀으나, 로그인이 되지 않은 경우에는 로그인페이지가 먼저 나온다.
 // todo 5. 로그인이 되어있을 경우에는 상품디테일 페이지를 볼 수 있다.
 // todo 6. 로그아웃 버튼을 클릭하면 로그아웃이 된다.
 // todo 7. 로그아웃이 되면 상품 디테일 페이지를 볼 수 없다. 다시 로그인 페이지가 보인다.
@@ -32,7 +33,10 @@ function App() {
           path="/login"
           element={<Login setAuthenticate={setAuthenticate} />}
         />
-        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route
+          path="/product/:id"
+          element={<PrivateRoute authenticate={authenticate} />}
+        />
       </Routes>
     </div>
   );
