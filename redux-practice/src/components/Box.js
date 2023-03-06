045a8faa2cf9b28fb38d7c6a7272773f33bee025
yurimpark <yuriming112@gmail.com>
2023-03-06 15:30:40 +0900

    주석 추가

--- redux-practice/src/components/Box.js
+++ redux-practice/src/components/Box.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import GrandSonBox from "./GrandSonBox";
 
 const Box = () => {
+  // useSelector를 사용하면 props로 넘겨주고 받을 필요가 없다.
   let count = useSelector(state => state.count);
   return (
     <div>
--- redux-practice/src/components/GrandSonBox.js
+++ redux-practice/src/components/GrandSonBox.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const GrandSonBox = () => {
+  // useSelector를 사용하면 props로 넘겨주고 받을 필요가 없다.
   let count = useSelector(state => state.count);
   return <div>GrandSonBox {count}</div>;
 };
