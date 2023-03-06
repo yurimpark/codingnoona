9b48f56afa2d5e8f809af84cf6a9cb1b9363eca9
yurimpark <yuriming112@gmail.com>
2023-03-06 15:51:10 +0900

    payload, 감소버튼 과제

--- redux-practice/src/App.js
+++ redux-practice/src/App.js
@@ -8,12 +8,18 @@ function App() {
   const count = useSelector(state => state.count);
 
   const increase = () => {
-    dispatch({ type: "INCREMENT" });
+    // payload는 함수의 매개변수 같은 느낌
+    dispatch({ type: "INCREMENT", payload: { num: 5 } });
+  };
+
+  const decrease = () => {
+    dispatch({ type: "DECREMENT", payload: { num: 2 } });
   };
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={increase}>증가!</button>
+      <button onClick={decrease}>감소!</button>
       <Box />
     </div>
   );
