875612fce91ec1d52e867141b1f85b59d78820b2
yurimpark <yuriming112@gmail.com>
2023-03-06 15:19:26 +0900

    redux로 숫자 카운터 만들기

--- redux-practice/src/App.js
+++ redux-practice/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  // state를 매개변수로 줌 (store에 있는 모든 state를 전달)
+  const count = useSelector(state => state.count);
+
+  const increase = () => {
+    dispatch({ type: "INCREMENT" });
+  };
   return (
-    <div >
-    
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increase}>증가!</button>
     </div>
   );
 }
---
+++ redux-practice/src/redux/reducer/reducer.js
@@ -0,0 +1,18 @@
+let initialState = {
+  count: 0,
+};
+
+function reducer(state = initialState, action) {
+  console.log("액션은", action);
+  if (action.type === "INCREMENT") {
+    // reducer는 항상 return을 해줘야 함 (state한테 뭔가를 돌려줘야 함)
+    return { ...state, count: state.count + 1 };
+    // spread 문법을 통해 기존 객체 내용을 복사해서 새로운 객체에 전달하고,
+    // 변경하고 싶은 내용은 뒤에 따로 써주기
+  }
+  // store는 return이 무조건 있어야 한다
+  // 그래서 reducer에서 무조건 기본 리턴 필요
+  return { ...state };
+}
+
+export default reducer;
